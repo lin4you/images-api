b0a2f06b42cfbd8dafbe3e4403eab3cc74799b9c
Friedrich Waack <ubuntuuser1994!>
2019-02-14 09:15:51 +0100

    change App.vue layout to default vuetify layout markup

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,17 +1,19 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card card-default">
-                    <div class="card-header">Example Component</div>
-
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <v-app dark>
+        <v-toolbar app>
+            <v-toolbar-title>Image-API</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-toolbar-items class="hidden-sm-and-down">
+                <v-btn flat>Upload image</v-btn>
+            </v-toolbar-items>
+        </v-toolbar>
+        <v-content>
+            <v-container fluid>
+                <h1>content</h1>
+            </v-container>
+        </v-content>
+        <v-footer app></v-footer>
+    </v-app>
 </template>
 
 <script>
